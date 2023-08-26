c5e53e1f3d2a01a4c0ae9e37fe2ca6f750a99d9e
MiraDov290 <muzhyk.myroslava@gmail.com>
2023-08-26 18:59:45 +0100

    changs

--- src/components/CryptoHistory/CryptoHistory.jsx
+++ src/components/CryptoHistory/CryptoHistory.jsx
@@ -1,5 +1,27 @@
 import { BaseTable, THead, Th, Tr, Td } from './CryptoHistory.styled';
 
-export const CryptoHistory = () => {
-  return <div>CryptoHistory</div>;
+export const CryptoHistory = ({ items }) => {
+  return (
+    <BaseTable>
+  <THead>
+    <tr>
+      <Th>№</Th>
+      <Th>PRICE</Th>
+      <Th>AMOUNT</Th>
+      <Th>DATE</Th>
+    </tr>
+  </THead>
+
+      <tbody>
+        {items.map(({ id, price, amount, date }) => (
+<Tr key={id}>
+            <Td>1</Td>
+            <Td>{price}</Td>
+            <Td>{amount}</Td>
+            <Td>{date}</Td>
+    </Tr>
+        ))}
+  </tbody>
+</BaseTable>
+  )
 };
