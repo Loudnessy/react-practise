73e1a5d19a618d700b204179538a572562a8951a
OlegBarabash <olegbarabash7@gmail.com>
2023-08-26 12:34:05 +0300

    task-4

--- src/service/service.js
+++ src/service/service.js
@@ -1,6 +1,9 @@
-import { formatDistanceToNow } from 'date-fns'
-
+import { formatDistanceToNow, format } from 'date-fns';
 
 export const formatter = data => {
-    return formatDistanceToNow(new Date(data), { addSuffix: true })
-}
+  return formatDistanceToNow(new Date(data), { addSuffix: true });
+};
+
+export const formatTable = date => {
+  return format(new Date(date), 'Pp');
+};
